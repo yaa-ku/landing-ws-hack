86aa750f0ab990e903f0e58953a74c6ebc759216
yanakuz <yana.yana.kuznecova666@gmail.com>
2023-10-06 12:12:58 +1000

    fonts and color settings

--- src/components/studentsBlock/studentsBlock.tsx
+++ src/components/studentsBlock/studentsBlock.tsx
@@ -8,7 +8,15 @@ type Props = {
 export const StudentsBlock = (props: Props) => {
     return (
         <div>
-            <p>student block</p>
+            <h1 className={'title-1'}>h1</h1>
+            <h2 className={'title-2--primary'}>h2</h2>
+            <h2 className={'title-2--secondary'}>h2</h2>
+            <h3 className={'title-3--primary'}>h3</h3>
+            <h3 className={'title-3--secondary'}>h3</h3>
+            <p className={'body-1--primary'}>body 1</p>
+            <p className={'body-1--secondary'}>body 1</p>
+            <p className={'body-2'}>body 2</p>
+            <p className={'link'}>link</p>
         </div>
     );
 };
