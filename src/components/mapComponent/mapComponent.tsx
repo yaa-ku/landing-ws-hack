96f6bb331d99b33816b2796673bfcb66cce54a7e
bavtim <40119744+bavtim@users.noreply.github.com>
2023-10-06 08:06:34 +0300

    update map
    update map

--- src/components/mapComponent/mapComponent.tsx
+++ src/components/mapComponent/mapComponent.tsx
@@ -1,53 +1,116 @@
 'use client'
 import React, {useEffect, useState} from 'react';
 import {YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
-
+import './map.scss';
+async function get_request_api<T>(url: string): Promise<T> {
+    console.log("API REQUEST")
+    console.log(url)
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                console.log("API RESPONSE BAD")
+                throw new Error(response.statusText)
+            }
+            console.log("API RESPONCE OK")
+            return response.json() as Promise<T>
+        })
+}
 
 const MapComponent: React.FC = () => {
-
-    interface TotalUsers {
-        id: number,
-        coordinates: Array<number>,
-        name: string,
-        iconUrl: string,
+    interface Point {
+        longitude: number,
+        latitude: number,
+        address: string
     }
 
-    const [markers, setMarkers] = useState<Array<TotalUsers>>();
+    interface Partner {
+        id: number,
+        title: string,
+        iconUrl: string,
+        point: Point,
+    }
 
+    interface Service {
+        title: string
+    }
+
+    interface PartnerDescription {
+        id: number,
+        title: string,
+        description: string,
+        iconUrl: string,
+        point: Point,
+        services: Array<Service>,
+    }
+
+    const [markers, setMarkers] = useState<Array<Partner>>();
+    const [currentPartner, setCurrentPartner] = useState<PartnerDescription>();
     useEffect(() => {
-        const fakeMarkerData: Array<TotalUsers> = [
-            {id: 1, coordinates: [48.76, 135.03], name: 'Москва', iconUrl: 'https://ht.dicamp.ru/uploads/Logo_Background_Main_Or_e6f3c74fa5.jpg'},
-            {id: 2, coordinates: [48.58, 135.05], name: 'Санкт-Петербург', iconUrl: 'https://ht.dicamp.ru/uploads/Logo_Background_Main_Or_e6f3c74fa5.jpg'},
-        ];
+        const fetchData = async () => {
+            let data = await get_request_api<Array<Partner>>("https://ht.dicamp.ru/api/partners/list")
+            console.log(data)
+            setMarkers(data);
+        }
+        fetchData()
+    }, [])
 
-        setMarkers(fakeMarkerData);
-    }, []);
+
+    function handleMarkerClick(marker: Partner) {
+        const fetchData = async () => {
+            let data = await get_request_api<PartnerDescription>("https://ht.dicamp.ru/api/partners/" + marker.id)
+            console.log(data)
+            setCurrentPartner(data);
+        }
+        fetchData()
+        console.log(marker)
+    }
 
     return (
-        <YMaps>
-            <Map
-                defaultState={{
-                    center: [48.51578, 135.5117],
-                    zoom: 10,
-                }}
-                style={{width: '400px', height: '400px'}}
-            >
-                {markers?.map((marker: TotalUsers) => (
-                    <Placemark
-                        key={marker.id}
-                        geometry={marker.coordinates}
-                        properties={{
-                            hintContent: marker.name,
+        <div className="horizontal-split-container">
+            <div className="left-panel">
+                <YMaps>
+                    <Map
+                        defaultState={{
+                            center: [48.51578, 135.5117],
+                            zoom: 10,
                         }}
-                        options={{
-                            iconLayout: 'default#image',
-                            iconImageHref: marker.iconUrl, // Используем URL-адрес изображения иконки с сервера
-                            iconImageSize: [30, 30],
-                        }}
-                    />
-                ))}
-            </Map>
-        </YMaps>
+                        style={{width:'100%' , height: '100%'}}
+                    >
+                        {markers?.map((marker: Partner) => (
+                            <Placemark
+                                key={marker.id}
+                                geometry={[marker.point.longitude, marker.point.latitude]}
+                                properties={{
+                                    hintContent: marker.title,
+                                }}
+                                options={{
+                                    iconLayout: 'default#image',
+                                    // iconContentLayout: '<div style="background-color: blue; width: 20px; height: 20px;"></div>',
+                                    iconImageHref: "https://ht.dicamp.ru/" + marker.iconUrl, // Используем URL-адрес изображения иконки с сервера
+                                    iconImageSize: [60, 30], // Установите размер как [60, 60] для круглой иконки
+                                    // iconContentOffset: [0, 0],
+                                }}
+                                onClick={() => handleMarkerClick(marker)}
+                            />
+                        ))}
+                    </Map>
+                </YMaps>
+
+            </div>
+            <div className="right-panel">
+                <div className="title-img-container">
+                    <img className={'image'} src={`https://ht.dicamp.ru/${currentPartner?.iconUrl}`} alt=""/>
+                    <div className={'name-address'}>
+                        <h1>{currentPartner?.title}</h1>
+                        <p>{currentPartner?.point.address}</p>
+                    </div>
+
+                </div>
+                <p>{currentPartner?.description}</p>
+
+
+            </div>
+        </div>
     );
 };
 
